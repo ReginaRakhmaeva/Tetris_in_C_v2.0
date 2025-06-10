ba242e0c3a49cbc63f7f23c827ce77c168b55976
branstod <branstod@DESKTOP-6QGP4OI.localdomain>
2025-06-10 21:31:10 +0300

    настройка первичных файлов

---
+++ src/brick_game/brick_game/brick_game.h
@@ -0,0 +1,13 @@
+#ifndef BRICKGAME_H
+#define BRICKGAME_H
+
+/** @brief Количество строк в игровом поле. */
+#define ROWS 20
+
+/** @brief Количество столбцов в игровом поле. */
+#define COLS 10
+
+/** @brief Код успешного выполнения. */
+#define SUCCESS 0
+
+#endif  // BRICKGAME_H
