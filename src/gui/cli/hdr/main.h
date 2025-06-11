a3a934c0a5abe481e28f8aee504efc2c6ed86714
branstod <branstod@DESKTOP-6QGP4OI.localdomain>
2025-06-11 15:39:45 +0300

    настройка фронтовых файлов main

---
+++ src/gui/cli/hdr/main.h
@@ -0,0 +1,14 @@
+/**
+ * @file main.h
+ * @brief Заголовочный файл с основными функциями для инициализации и управления
+ * игрой.
+ */
+
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <locale.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#endif  // MAIN_H
