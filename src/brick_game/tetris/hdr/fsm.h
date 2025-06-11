7a2d51fecb4190caadba9fa2aa633e43dcbf4756
branstod <branstod@DESKTOP-6QGP4OI.localdomain>
2025-06-11 16:09:06 +0300

    настройка файлов fsm

---
+++ src/brick_game/tetris/hdr/fsm.h
@@ -0,0 +1,12 @@
+/**
+ * @file fsm.h
+ * @brief Определение функций и структур для обработки состояний игры и ввода
+ * пользователя.
+ */
+
+#ifndef FSM_H
+#define FSM_H
+
+#include "main.h"
+
+#endif  // FSM_H
